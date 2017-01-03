91a9cdb88d61f468019aa90170cd7bffd5b277d9
vika <vika@mail.com>
2017-01-04 01:59:33 +0300

    add BookManger3

---
+++ BookManager3/src/main/java/net/proselyte/bookmanager/controller/LoginController.java
@@ -0,0 +1,62 @@
+package net.proselyte.bookmanager.controller;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
+
+import net.proselyte.bookmanager.objects.User;
+
+@Controller
+@SessionAttributes("user")
+public class LoginController {
+
+    private static final int WEAK_STRENGTH = 1;
+    private static final int FEAR_STRENGTH = 5;
+    private static final int STRONG_STRENGTH = 7;
+
+    private static final String WEAK_COLOR = "#FF0000";
+    private static final String FEAR_COLOR = "#FF9900";
+    private static final String STRONG_COLOR = "#0099CC";
+
+    @Autowired
+    private MessageSource messageSource;
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @ModelAttribute
+    public User createNewUser() {
+        return new User("usernamevalue");
+    }
+
+    @RequestMapping(value = "/downloadPDF", method = RequestMethod.GET)
+    public ModelAndView downloadPDF() {
+
+        return new ModelAndView("pdfView");
+    }
+
+}
